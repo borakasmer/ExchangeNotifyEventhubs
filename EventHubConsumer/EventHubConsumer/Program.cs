36d0e44e4aefbc8db40c80a53bd031abffbe337c
Bora Kasmer <bora@borakasmer.com>
2019-04-03 21:41:08 +0300

    Create Project.

---
+++ EventHubConsumer/EventHubConsumer/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.Azure.EventHubs.Processor;
+using Microsoft.Azure.EventHubs;
+
+namespace EventHubConsumer
+{
+    class Program
+    {        
+        private const string EventHubConnectionString = "Endpoint=sb://exchangenotifyeventhub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=**********************";
+        private const string EventHubName = "exchangeeventhub";
+
+        private const string StorageContainerName = "exchangehubcontainer";
+        private const string StorageAccountName = "exchangecontainer";
+        private const string StorageAccountKey = "****************";
+
+        private static readonly string StorageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", StorageAccountName, StorageAccountKey);
+
+        private static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Processor Ayağa Kalkıyor...");
+
+            var eventProcessorHost = new EventProcessorHost(
+                EventHubName,
+                PartitionReceiver.DefaultConsumerGroupName,
+                EventHubConnectionString,
+                StorageConnectionString,
+                StorageContainerName);
+
+            // Event Processor mesaj almaya başlaması için Host Ediliyor.
+            await eventProcessorHost.RegisterEventProcessorAsync<Processor>();
+
+            Console.WriteLine("Yeni paket bekleniyor. Durdurmak için Lütfen Bir Tuşa Basın.");
+            Console.ReadLine();
+
+            // Disposes of the Event Processor Host    
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+        public class Processor : IEventProcessor
+        {
+            public Task CloseAsync(PartitionContext context, CloseReason reason)
+            {
+                Console.WriteLine($"Process Kapanıyor. Partition '{context.PartitionId}', Sebep: '{reason}'.");
+                return Task.CompletedTask;
+            }
+
+            public Task OpenAsync(PartitionContext context)
+            {
+                Console.WriteLine($"Processor Ayağa Kalkıyor. Partition: '{context.PartitionId}'");
+                return Task.CompletedTask;
+            }
+
+            public Task ProcessErrorAsync(PartitionContext context, Exception error)
+            {
+                Console.WriteLine($"Partition'da Hata Var: {context.PartitionId}, Hata: {error.Message}");
+                return Task.CompletedTask;
+            }
+
+            public Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+            {
+                foreach (var eventData in messages)
+                {
+                    var data = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+                    Console.WriteLine($"Message alındı. Partition: '{context.PartitionId}', Kur Değeri: '{data}'");
+                }
+
+                return context.CheckpointAsync();
+            }
+        }
+    }
+}
---
+++ ExchangeEventHubs/ExchangeEventHubs/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ExchangeEventHubs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerateRandomMessages();
+        }
+        public static void GenerateRandomMessages()
+        {
+            var primaryConnectionString = "Endpoint=sb://exchangenotifyeventhub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=***************************";
+            var client = EventHubClient.CreateFromConnectionString(primaryConnectionString, "exchangeeventhub");
+            Random randomNumberGenerator = new Random();
+
+            int counter = 100;
+            while (true)
+            {
+                try
+                {
+                    counter++;
+                    // generate any random numbet between 1 and 1000  
+                    var randomMessage = string.Format("Son Güncel Kur {0}", 5.4 + (double)(randomNumberGenerator.Next(100, 1000))/1000);
+
+                    Console.WriteLine("Generated message: {0}", randomMessage);
+
+                    client.Send(new EventData(Encoding.UTF8.GetBytes(randomMessage)));
+                    if(counter>110) { return; }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+
+                Thread.Sleep(1000);                
+            }
+            Console.ReadLine();
+        }
+    }
+}
